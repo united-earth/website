5033d4291230a847acaec3fe085358b13d30e251
Nicolas Luck <nicolas@lucksus.eu>
2015-09-03 20:06:47 +0200

    HAML, Sass, Ruby version

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module UnitedEarth
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.sass.preferred_syntax = :sass
   end
 end
