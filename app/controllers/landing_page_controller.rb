b0df0245697eae875f1ec1fa817d19363f477283
Nicolas Luck <nicolas@lucksus.eu>
2015-09-03 20:35:36 +0200

    I18n stuff

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,7 +1,6 @@
 class LandingPageController < ApplicationController
   def locale_redirect
+    redirect_to landing_page_path(locale: http_accept_language.compatible_language_from(I18n.available_locales))
   end
 
-  def index
-  end
 end
