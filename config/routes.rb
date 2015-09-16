f6a20821cda61c68eaeac12eb612d05013873264
Nicolas Luck <nicolas@lucksus.eu>
2015-09-16 23:07:32 +0200

    UN application WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   scope ":locale", locale: /#{I18n.available_locales.join("|")}/, path_prefix: "/:locale" do
-    root 'landing_page#index',                  as: :landing_page
+    root 'landing_page#index',                              as: :landing_page
+    get 'un-application' => 'landing_page#un_application',  as: :un_application
   end
 
   get '/' => 'landing_page#locale_redirect'
