e1cafd13d751ae72ddd38da12d5569babbc2f71f
Nicolas Luck <nicolas@lucksus.eu>
2015-09-20 22:21:20 +0200

    Meta tags

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'bootstrap-sass', '~> 3.3.5'
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'simple_form'
+gem 'meta-tags'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,28 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :set_locale
+  before_filter :set_meta
 
   def set_submenu
     @has_submenu = true
   end
 
+
   private
   def set_locale
     I18n.locale = params[:locale]
+  end
+
+  def set_meta
+    set_meta_tags title: 'United Earth',
+                  description: 'United Earth extends a planetary invitation to all life-affirming persons and groups to unite and collaborate for a whole-systems transformation of human society.',
+                  keywords: 'United, Earth, Co-creation, Unity, Collaboration, Transformation, Whole-System'
+
+    set_meta_tags og: {
+                      title:    'United Earth',
+                      url:      'http://united-earth.vision',
+                      image:    'http://united-earth.vision/logo.png',
+                      description: 'United Earth extends a planetary invitation to all life-affirming persons and groups to unite and collaborate for a whole-systems transformation of human society.'
+                  }
   end
 end
