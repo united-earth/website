cfedb40f972e5621f0d48f19e8af7521abe89f00
Nicolas Luck <nicolas@lucksus.eu>
2015-09-24 14:46:22 +0200

    Fix group links

--- app/helpers/landing_page_helper.rb
+++ app/helpers/landing_page_helper.rb
@@ -1,2 +1,8 @@
 module LandingPageHelper
+  def make_fullpath(path)
+    unless path.start_with?('http://') or path.start_with?('https://')
+      return "http://#{path}"
+    end
+    path
+  end
 end
