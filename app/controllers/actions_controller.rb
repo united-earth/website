90a69c3a3a2f6a93117c40dadb8f2a858806846e
Nicolas Luck <nicolas@lucksus.eu>
2016-01-25 12:19:45 +0100

    Metatags for UE Festival page

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -8,6 +8,18 @@ class ActionsController < ApplicationController
 
   def uefestival
     @at_uefestival = true
+    url = 'https://united-earth.vision/en/actions/uefestival'
+    image = 'https://united-earth.vision/EUF16_FBpost_1200x900.jpg'
+    set_meta_tags title: 'United Earth Festival 2016',
+                  description: 'The United Earth Festival 2016 is a pilot project to effectively showcase and gather the existing abundance of practical and grassroots solutions, blueprints, technical guidance, social architectures and whole­systems transition strategies on o​ne integral co­creative platform. By gathering all solutions and intentionally aligned groups in one place, we can make them more visible to the world. Thus, everyone with an Internet connection will be able to access and contribute to a community­based global solutions library a​nd get a synergistic overview of real­time global and local collaboration and resource sharing possibilities.',
+                  keywords: 'United, Earth, festival, movements, grassroots'
+
+    set_meta_tags og: {
+                      title:    'United Earth Festival 2016',
+                      url:      url,
+                      image:    image,
+                      description: 'The United Earth Festival 2016 is a pilot project to effectively showcase and gather the existing abundance of practical and grassroots solutions, blueprints, technical guidance, social architectures and whole­systems transition strategies on o​ne integral co­creative platform. By gathering all solutions and intentionally aligned groups in one place, we can make them more visible to the world. Thus, everyone with an Internet connection will be able to access and contribute to a community­based global solutions library a​nd get a synergistic overview of real­time global and local collaboration and resource sharing possibilities.'
+                  }
   end
 
   def gathering
