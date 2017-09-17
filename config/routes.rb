a51b0ad40a47b5cd4156a500bdce74df0a6dbabd
Nicolas Luck <nicolas@lucksus.eu>
2017-09-18 00:45:51 +0200

    Revert "redirect to temporary google website"
    This reverts commit cd82ad72c909c2a84f7f00f0647958184c16b497.

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,7 @@ Rails.application.routes.draw do
 
   scope ":locale", locale: /#{I18n.available_locales.join("|")}/, path_prefix: "/:locale" do
     devise_for :users, controllers: { registrations: 'registrations' }
-    #root 'landing_page#index',                                as: :landing_page
-    root to: redirect('https://sites.google.com/view/united-earth/home'), as: :landing_page
+    root 'landing_page#index',                                as: :landing_page
     get 'un-application'              => 'landing_page#un_application',         as: :un_application
     get 'settings'                    => 'user_settings#index',                 as: :user_settings
     post 'settings/user'              => 'user_settings#update_user',           as: :user_update
